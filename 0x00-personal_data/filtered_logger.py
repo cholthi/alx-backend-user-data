c6619e4098eec1be568735f2290f4ef2a8b9678a
cholthi <cholthipaul@gmail.com>
2024-04-14 10:45:33 +0200

    last commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,12 @@
 in the logs
 """
 import re
+import os
 from typing import List
+import logging
+from mysql.connector import connection
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def redact(redaction):
@@ -17,3 +22,70 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = r'(?P<field>{})=[^{}]*'.format(
                 '|'.join(fields), separator)
     return re.sub(pattern, redact(redaction), message)
+
+def get_logger() -> logging.Logger:
+    """ returns a named logger that redacts PII data"""
+    logger = logging.getlooger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    redact_formatter = RedactingFormatter(PII_FIELDS)
+    handler = logging.StreamHandler()
+    handler.setFormatter(redact_formatter)
+    logger.addHandler(handler)
+    return logger
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a handle to mysql database connection"""
+    user = os.environ.get('PERSONAL_DATA_DB_USERNAME')
+    password = os.environ.get('PERSONAL_DATA_DB_PASSWORD')
+    host = os.environ.get('PERSONAL_DATA_DB_HOST')
+    dbname = os.environ.get('PERSONAL_DATA_DB_NAME')
+    cnx = connection.MySQLConnection(user=user, password=password,
+                                 host=host,
+                                 database=dbname)
+    return cnx
+
+
+def main():
+    """Log the information about user records in a table.
+    """
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
+            )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Formats log message"""
+        msg = super(RedactingFormatter, self).format(record)
+        out = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return out 
+
+
+    if __name__ ==  '__main__':
+        main()
