216aae4f8777e8ddd62c21400278966a69de932c
cholthi <cholthipaul@gmail.com>
2024-04-14 07:15:40 +0200

    initial commit

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""provides filter_datum function that  obfuscates sensitive data
+in the logs
+"""
+import re
+from typing import List
+
+
+def redact(match, redaction):
+    """replacer function for re.sub"""
+    m = match.group(1)
+    return f'{m}={redaction}'
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """filter out sensitive data in the message"""
+    for field in fields:
+        pattern = fr'({re.escape(field)})=[^;]+'
+        msg = re.sub(pattern, lambda m: redact(m, redaction), message)
+    return msg
