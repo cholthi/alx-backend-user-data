45da5d6149055655c60c3c55374e69b976871da4
cholthi <cholthipaul@gmail.com>
2024-04-14 21:00:20 +0200

    added encrypt_password.py file

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" encrypt user password"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password using a random salt.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if a hashed password was formed from the given password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
