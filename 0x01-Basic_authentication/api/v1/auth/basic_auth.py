80e90b784ee0e3e13896840e71da78fcca46b966
cholthi <cholthipaul@gmail.com>
2024-04-16 16:32:08 +0200

    added decode_base64_authorization_header function to basic_auth.py file

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Basic authentication strategy"""
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -13,3 +15,15 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Decodes Authorization header value"""
+        if not base64_authorization_header or type(
+                base64_authorization_header) != str:
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode('utf-8')
+        except binascii.Error:
+            None
