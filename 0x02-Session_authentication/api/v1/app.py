9356cfc07d31d1dc3f3ab8b9bf3ff73da1a4bb4d
cholthi <cholthipaul@gmail.com>
2024-04-19 14:52:01 +0200

    initial commit- fixed errors

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,7 +35,7 @@ def filter_request():
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
-            request.user = auth.current_user(request)
+            request.current_user = auth.current_user(request)
 
 
 # @app.errorhandler(404)
