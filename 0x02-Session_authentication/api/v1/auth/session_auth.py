e0aa018d0514a45d94913fb74c80c2ee4b8633a8
cholthi <cholthipaul@gmail.com>
2024-04-21 14:40:11 +0200

    added function 'user_id_for_session_id' to retrieve user_id given session id string

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -16,3 +16,9 @@ class SessionAuth(Auth):
         sess_id = str(uuid.uuid4())
         SessionAuth.user_id_by_session_id[sess_id] = user_id
         return sess_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns user id given a session ID string"""
+        if session_id is None or type(session_id) != str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
