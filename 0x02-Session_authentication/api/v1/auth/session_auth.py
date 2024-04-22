452d277fa31f221d6333a364339941f1d03382b9
cholthi <cholthipaul@gmail.com>
2024-04-22 07:54:56 +0200

    added current user to session auth file

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -26,5 +26,5 @@ class SessionAuth(Auth):
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Return the current user based on session cookie"""
-        user_id = user_id_for_session_id(self.session_cookie(request))
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
