031739098f0b0368033129ac4e4478c091566bbb
cholthi <cholthipaul@gmail.com>
2024-04-22 16:30:15 +0200

    added user mapped class

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Mapped user data object """
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ Mapped user data class """
+    __tablename__: str = 'user'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
