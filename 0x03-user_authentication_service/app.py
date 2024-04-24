83583e2c4292bbe1904f51eb7951bb9e87bcd2b8
cholthi <cholthipaul@gmail.com>
2024-04-24 12:51:42 +0200

    added app instance variable

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3
 """ APP
 """
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from werkzeug.wrappers import Response
+from auth import Auth
 
 app = Flask(__name__)
-app.ur_map.strict_slashes = True
+app.url_map.strict_slashes = True
+auth = Auth()
 
 
 @app.route('/', methods=['GET'])
-def index():
+def index() -> Response:
     """ index view function
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users() -> Response:
+    """ Users
+    """
+    user_data = request.form
+    try:
+        user = auth.register_user(
+                user_data["email"], user_data["password"])
+        if user:
+            return jsonify({
+                "email": user.email,
+                "message": "user created"
+                })
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
