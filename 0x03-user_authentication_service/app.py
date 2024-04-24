bb943fb651da542503f37258e0af8fa186458122
cholthi <cholthipaul@gmail.com>
2024-04-24 12:18:35 +0200

    added app instance variable

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" APP
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.ur_map.strict_slashes = True
+
+@app.route('/', methods=['GET'])
+def index():
+    """ index view function
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
