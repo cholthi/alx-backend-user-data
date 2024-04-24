d2c55ad1da51439be8f35514bd5b0a5f78e8df52
cholthi <cholthipaul@gmail.com>
2024-04-24 14:04:26 +0200

    added create_session to Auth service class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,7 @@
 """Auth helper
 """
 import bcrypt
-from typing import TypeVar
+from typing import TypeVar, Union
 from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 import uuid
@@ -52,3 +52,15 @@ class Auth:
                 return False
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> Union[str, None]:
+        """ Create a user session and return the 
+        session identifier
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
