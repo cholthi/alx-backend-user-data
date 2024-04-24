aff3820337dd22fec76b526a55717ee593da6af2
cholthi <cholthipaul@gmail.com>
2024-04-24 13:44:37 +0200

    added valid_login method to Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -24,12 +24,22 @@ class Auth:
     def register_user(self, email: str, password: str) -> TypeVar('User'):
         """ Register a user to the user store
         """
-        if not email or not password:
-            raise ValueError('email or password can not be empty')
         try:
             _ = self._db.find_user_by(email=email)
             raise ValueError('User {} already exists'.format(email))
         except NoResultFound:
             user = self._db.add_user(email, _hash_password(
                 password).decode('utf-8'))
-            return user
+        return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ checks for valid user crendentials in db
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password.encode('utf-8')):
+                return True
+            else:
+                return False
+        except NoResultFound:
+            return False
